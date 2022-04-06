8ab3881ef985af596db2593c561f443c17a90935
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-04-05 21:43:39 -0400

    parse duration and message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
 use std::io::{BufRead, BufReader};
 use std::thread;
 use std::os::unix::net::{UnixStream, UnixListener};
+use std::str::FromStr;
+
+struct Args {
+    duration: String,
+    message: String,
+}
+
+static DEFAULT_MESSAGE: &'static str = "No Message";
+
+impl FromStr for Args {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let vec: Vec<&str> = s.split(' ').collect();
+        
+        match vec.len() {
+            0 => Err(()),
+            1 => {
+                let duration = vec[0].to_string();
+                let message = DEFAULT_MESSAGE.to_string();
+                Ok(Args { duration, message })
+            },
+            _ => {
+                let duration = vec[0].to_string();
+                let message = vec[1..vec.len()].join(" ");
+                Ok(Args { duration, message })
+            },
+        }
+    }
+}
 
 fn handle_client(stream: UnixStream) {
     let stream = BufReader::new(stream);
     for line in stream.lines() {
-        println!("{}", line.unwrap());
+        let Args {duration, message} = Args::from_str(&line.unwrap()).unwrap();
+        println!("duration: {}, message: {}", duration, message);
     }
 }
 
