e9819ad0b8669723d5265315892c51e18ac71ec2
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-04-05 21:10:36 -0400

    make a basic unix listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader};
+use std::thread;
+use std::os::unix::net::{UnixStream, UnixListener};
+
+fn handle_client(stream: UnixStream) {
+    let stream = BufReader::new(stream);
+    for line in stream.lines() {
+        println!("{}", line.unwrap());
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = UnixListener::bind("/tmp/rust-uds.sock").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| handle_client(stream));
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
+    Ok(())
 }
